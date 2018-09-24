1b3fc82825835a1e6b6ad1f44ff1c37fd0d11e7b
Marcelo Menezes <marcelodmmenezes@gmail.com>
2018-09-24 03:25:36 -0300

    Started partial sorting

--- Sorting/partialSorting.hpp
+++ Sorting/partialSorting.hpp
@@ -7,6 +7,49 @@
  *                                                                            *
  * Adaptations of some sorting algorithms for more efficient partial sorting. *
  * That is, given an arbitrary array with n elements,                         *
- * we wish to sort the first k items.                                         *
+ * we wish to sort the first k smallest items.                                *
  *                                                                            *
  *****************************************************************************/
+
+
+#ifndef PARTIAL_SORTING_HPP
+#define PARTIAL_SORTING_HPP
+
+
+//-------------------------------------------------------------- Selection sort
+template <typename T>
+void partialSelectionSort(T* arr, int k, int size) {
+}
+//-----------------------------------------------------------------------------
+
+//-------------------------------------------------------------- Insertion sort
+template <typename T>
+void partialInsertionSort(T* arr, int k, int size) {}
+//-----------------------------------------------------------------------------
+
+//------------------------------------------------------------------- Quicksort
+template <typename T>
+void partialQuickSort(T* arr, int left, int right) {}
+
+template <typename T>
+void partialQuickSort(T* arr, int k, int size) {}
+//-----------------------------------------------------------------------------
+
+//------------------------------------------------------------------ Merge sort
+template <typename T>
+void partialMergeSort(T* arr, T* merge_array, int left, int right) {}
+
+template <typename T>
+void partialMergeSort(T* arr, int k, int size) {}
+//-----------------------------------------------------------------------------
+
+//-------------------------------------------------------------------- Heapsort
+template <typename T>
+void partialHeapify(T* arr, int left, int right) {}
+
+template <typename T>
+void partialHeapSort(T* arr, int k, int size) {}
+//-----------------------------------------------------------------------------
+
+
+#endif	// PARTIAL_SORTING_HPP
