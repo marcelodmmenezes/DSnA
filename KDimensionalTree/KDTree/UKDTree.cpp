a2e4655830ed530b73400ec89655b2c1bb18ffcd
marcelodmmenezes <marcelodmmenezes@gmail.com>
2018-10-01 13:26:33 -0300

    Started k-d tree

---
+++ KDimensionalTree/KDTree/UKDTree.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * INF 610 - Estruturas de Dados e Algoritmos - 2018/2                        *
+ *                                                                            *
+ * Trabalho prático 2                                                         *
+ *                                                                            *
+ * Marcelo de Matos Menezes - 75254                                           *
+ *                                                                            *
+ * Just a simple program to test UKDTree implementation.                       *
+ *                                                                            *
+ *****************************************************************************/
+
+
+#include "UKDTree.hpp"
+
+
+int main() {
+	UKDTree<int> t;
+}
---
+++ KDimensionalTree/KDTree/UKDTree.hpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * INF 610 - Estruturas de Dados e Algoritmos - 2018/2                        *
+ *                                                                            *
+ * Trabalho prático 2                                                         *
+ *                                                                            *
+ * Marcelo de Matos Menezes - 75254                                           *
+ *                                                                            *
+ * Umbalanced K-D Tree implementation.                                        *
+ *                                                                            *
+ *****************************************************************************/
+
+
+#ifndef UKD_TREE_HPP
+#define UKD_TREE_HPP
+
+
+template <typename T, size_t K>
+struct UKDTNode {
+
+};
+
+
+template <typename T, size_t K>
+class UKDTree {
+public:
+
+
+private:
+};
+
+
+#endif	// KD_TREE_HPP
